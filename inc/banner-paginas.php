d65f1b1825cac5e505ec375092928b8fd7f111e0
Ana Flavia Cador <aflaviah15@gmail.com>
2019-05-15 12:55:54 -0300

    dev

--- inc/banner-paginas.php
+++ inc/banner-paginas.php
@@ -6,4 +6,10 @@ if(! is_front_page()) { $bannerclass = ' class="pagina-interna"'; }
 
 echo '<div id="banner"'.$bannerclass.'>';
 	if ($bannerhome) { echo '<img src="'.$bannerhome.'">'; }
+	if(! is_front_page()) {
+		echo '<div class="container">';
+			global $post; 
+			echo '<h1>'.get_the_title($post->ID).'</h1>';
+		echo '</div>';
+	}
 echo '</div>';
